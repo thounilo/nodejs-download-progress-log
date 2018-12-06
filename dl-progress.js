80d90f7a3355159737d7af45ebf42cb5ac17a614
RoboSapienssi <thounilo@gmail.com>
2018-12-06 09:18:23 +0200

    Create dl-progress.js

---
+++ dl-progress.js
@@ -0,0 +1,42 @@
+const readline = require('readline');
+const chalk = require('chalk');
+
+const createDownloadProgress = ({
+  length = 0,
+  unit = 'kb'
+}) => {
+
+  const roundDecimal = (value, decimals) => {
+    return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+  }
+
+  cosnt transformBytes = (bytes, type) => {
+    const unit = {
+      kb: 1e+3,
+      mb: 1e+6,
+      gb: 1e+9
+    }
+    return roundDecimal(bytes / unit[type], 2)
+  }
+  
+  let downloaded = 0
+
+  return ({
+    buffer = new Buffer(0), 
+    message = '', 
+    complete = false
+  }) => {
+
+    readline.cursorTo(process.stdout, 0);
+    readline.clearLine(process.stdout, 1);
+    downloaded += buffer.length
+
+    if( !complete ) {
+      process.stdout.write(chalk.blue(`${ message } ${ transformBytes(downloaded, unit) } / ${ transformBytes(length, unit) }${ unit }`))
+    } else {
+      process.stdout.write(chalk.green(complete + " " + transformBytes(length, unit) + "" + unit + "\n" ))
+    }
+  }
+}
+
+module.exports = createDownloadProgress
